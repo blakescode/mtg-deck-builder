e6913de8cff15cce1ce11b68a4851f5666630845
blakescode <rydyell8@gmail.com>
2021-12-21 09:43:56 +0000

    add hover tooltip, fix some image file naming

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -1,11 +1,21 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { getImageUrl } from '../../helpers/images'
 import './card.css'
 
 const Card = ({ card }) => {
+  const [showTooltip, setShowTooltip] = useState(false);
   return (
-    <div className='card-container'>
+    <div
+      className='card-container'
+      onMouseEnter={() => setShowTooltip(true)}
+      onMouseLeave={() => setShowTooltip(false)}
+    >
       <img src={getImageUrl(card)} alt={card.name} className='card-image'/>
+      {showTooltip && (
+        <div className='tooltip-container'>
+          <img src={getImageUrl(card)} alt={card.name} className='card-tooltip'/>
+        </div>
+      )}
     </div>
   )
 }
